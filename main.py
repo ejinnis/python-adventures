8b1add89ecc0bbc7622a59b68e5bb3c2beb855ba
RadErik <102505790+RadErik@users.noreply.github.com>
2022-11-03 18:40:02 -0500

    Added section 5 to main

--- main.py
+++ main.py
@@ -454,6 +454,366 @@ def threeBats():
   else:
     invalid(threeBats)
 
+ # ======SECTION FIVE======
+
+# Start of section 5
+
+
+def fiveStart():
+    path = input(
+        "\nYou go east towards a large open field.  The air smells crisp and the grass is damp with dew. In the distance, you see a lake and cabin. The cabin seems to be abandoned. Where do you go?\n \n CABIN \n \n LAKE \n \n"
+    )
+    if path.lower() == "cabin":
+        print(
+            "\nGoing to the cabin.\n"
+        )
+        cabinPath()
+    elif path.lower() == "lake":
+        print(
+            "\nGoing to the lake.\n"
+        )
+        lakePath()
+    else:
+        invalid(fiveStart)
+
+# Cabin path part uno
+
+
+def cabinPath():
+    path = input(
+        "\n The cabin door seems to be boarded shut. The windows seem to be boarded shut too. What do you want to do?\n \n BREAK \n \n LEAVE"
+    )
+    if "break" in path.lower():
+        breakBoards()
+    elif "leave" in path.lower():
+        print(
+            "\nLeaving the cabin\n"
+        )
+        fiveStart()
+    else:
+        invalid(cabinPath)
+
+
+def breakBoards():
+    breakBoard = input(
+        "\nBreak it with what?\n \n FIST \n \n WEAPON \n"
+    )
+    if "fist" in breakBoard.lower():
+        roll = dice(15)
+        if roll[1] == True:
+            print(
+                "The boards have been broken. \n You have rolled: " + str(roll[0]))
+            print(
+                "\nEntering the cabin\n"
+            )
+            cabinPathPartTwo()
+
+        elif roll[1] == False:
+            print(
+                "The boards haven't been broken. Try again. \n You have rolled: " + str(roll[0]))
+            print("-1 HP")
+            rollTwo = dice(20)
+            if rollTwo[1] == True:
+                print("You hit your " + breakBoard.lower(
+                ) + " too hard, which cut it and broke it. You slowly bled to death. Come on man.")
+                gameOver()
+            breakBoards()
+    # !!! make sure to change "otherweapon" !!!
+        elif "weapon" in breakBoard.lower():
+            roll = dice(10)
+            if roll[1] == True:
+                print(
+                    "The boards have been broken. \n You have rolled: " + str(roll[0]))
+                print(
+                    "\nEntering the cabin.\n"
+                )
+                cabinPathPartTwo()
+        elif roll[1] == False:
+            print(
+                "The boards haven't been broken, try again. \n You have rolled: " + str(roll[0]))
+            breakBoards()
+    else:
+        invalid(breakBoards)
+
+# Part two of the cabin path
+
+
+def cabinPathPartTwo():
+    choosePath = input(
+        "The atmosphere is damp and dusty. To the left is a door frame. On the table in front of you are three cans. You're not sure if they're expired or not. Taking them may or may not be a good idea.\n TAKE \n \n DON'T \n"
+    )
+
+    if "take" in choosePath.lower():
+        print(
+            "\n Taking the cans"
+        )
+        cans()
+        cabinPathDoorFrame()
+    elif "don't" in choosePath.lower():
+        print(
+            "\nNot taking the cans."
+        )
+        cabinPathDoorFrame()
+    else:
+        invalid(cabinPathPartTwo)
+
+# Enter the door frame or not?
+
+
+def cabinPathDoorFrame():
+    enterDoor = input(
+        "Are you going through the doorframe?\n YES \n \n NO \n"
+    )
+    if "yes" in enterDoor:
+        print(
+            "entering the door"
+        )
+        cabinPathEnd()
+    elif "no" in enterDoor:
+        print(
+            "leaving the cabin")
+        fiveStart()
+    else:
+        invalid(cabinPathDoorFrame)
+
+# End of the cabin path
+
+
+def cabinPathEnd():
+    while True:
+        itemList = ["KNIFE", "CLOTHES", "LEAVE"]
+        print(itemList)
+        print(
+            "\nYou enter the doorway. The room seems to be a bedroom. It has a nice set of clothes laid out on the bed, like it was waiting for you. On top of the drawer is a knife. The knife has some strange symbols inscribed on it.\n"
+        )
+        itemGrab = input(
+            "What do you grab? \n \n" +
+            itemList[0] + "\n \n" + itemList[1] +
+            "\n \n" + itemList[2] + "\n \n"
+        )
+        if "knife" in itemGrab.lower():
+            print(
+                "Grabbing the knife."
+            )
+            # Popping it out doesn't work, figure out why
+            itemList.pop(0)
+            continue
+            # This works, but since it's a loop, the user can keep gaining more and more health, sooooo.
+        elif "clothes" in itemGrab.lower():
+            print(
+                "Grabbing the clothes. \n \n + 15HP"
+            )
+            hp(0, "edit", 15)
+            # Popping doesn't work
+            itemList.pop(1)
+            continue
+        elif "leave" in itemGrab.lower():
+            print(
+                "\nYou leave the cabin. You go back west, coming back to the open field\n"
+            )
+            fiveStart()
+        else:
+            invalid(cabinPathEnd)
+
+# Cans - They deal either -5 or +5 HP
+
+
+def cans():
+    print("Theses are the cans")
+
+
+# Part one of the lake path
+def lakePath():
+    path = input(
+        "\nIn front of you lay a beautiful disc of blue. The clouds above reflected just as well as the trees beside the lake. In the middle of the lake is a small raft, with something unknown on it. On the shore of the lake, is a small building. \nWhere do you go?\n \n LAKE \n \n BUILDING\n\n"
+    )
+    if "lake" in path.lower():
+        print(
+            "\nGoing swimming in the lake.\n"
+        )
+        swimmingPath()
+    elif "building" in path.lower():
+        print(
+            "\nGoing to the building\n"
+        )
+        buildingPath()
+    else:
+        invalid(lakePath)
+
+# Swimming path, dead end
+
+
+def swimmingPath():
+    path = input(
+        "\nYou take a nice swim in the lake, the water feels cool and refreshing. You're feeling kind of tired, you should probably go back to shore.\n\n SWIM \n\n GO BACK"
+    )
+    if "swim" in path.lower():
+        print(
+            "\nDespite feeling tired, you kept swimming. You feel something touch your leg, when. Oh no! A cramp! You try your best to swim back to shore, sucking in more water and more water. You feel as if you're about to make it to shore when something wraps around your leg. You get pulled into the lake by an unknown entity. The last image you see is the water-distorted sky.\n"
+        )
+        gameOver()
+    elif "go back" in path.lower():
+        print(
+            "\nGoing back\n"
+        )
+        lakePath()
+    else:
+        invalid(swimmingPath)
+
+# Building path, this is a one way, no escape
+
+
+def buildingPath():
+    path = input(
+        "\nInside the building, which is surprisingly clean, there's a table with a fishing rod. Beside the table is a large raft and paddle.\n\n GRAB FISHING ROD \n\n GRAB RAFT\n\n"
+    )
+    if "fishing rod" in path.lower():
+        print(
+            "\nWhat are you gonna do with a fishing rod, huh?\n"
+        )
+        buildingPath()
+    elif "raft" in path.lower():
+        print(
+            "\nYou pull the raft out of the building, and put it on the shore\n"
+        )
+        buildingPathPartTwo()
+    else:
+        invalid(buildingPath)
+
+# Fight the sea monster or not?
+
+
+def buildingPathPartTwo():
+    path = input(
+        "\nYou use the raft and go to the other raft on the lake. Once you start to get closer, you see what looks like a person on the raft. The person is unconscious. You bring them on your raft. Waking the person up, you ask them their name. 'Mipha', they answer. You're about to ask Mipha something else, when suddenly, something brushes against your boat. A giant sea monster pops out of the lake! What do you do?\n\n FIGHT \n\n PADDLE AWAY \n\n"
+    )
+    if "fight" in path.lower():
+        print(
+            "\nYou decide to stand your ground and fight the monster\n"
+        )
+        fightSeaMonst()
+    elif "paddle" in path.lower():
+        print(
+            "\nYou start paddling away, but the sea monster grabs you, tearing you both apart.\n"
+        )
+        gameOver()
+    else:
+        invalid(buildingPathPartTwo)
+
+# Fighting the sea monster
+
+
+def fightSeaMonst():
+    seaMonst = 100
+    mipha = 65
+    round = 1
+    print(
+        "\nYou and mipha fight the sea monster\n"
+    )
+    print("\n\n======ROUND 1======\n\n")
+    while True:
+        roll = dice(10)
+        # Link's attack
+        if roll[0] >= 10:
+            print(
+                "\nYou rolled a staggering " +
+                str(roll[0]) + ", That means you dealt 20 HP to the monster!\n"
+            )
+            seaMonst = seaMonst - int(roll[0])
+        elif roll[0] >= 5:
+            print(
+                "\nYou rolled " +
+                str(roll[0]) +
+                ", That means you only dealt 10 HP to the monster!\n"
+            )
+            seaMonst = seaMonst - int(roll[0])
+        elif roll[0] < 5:
+            print(
+                "\nYou rolled a measly " +
+                str(roll[0]) +
+                ", That means you didn't even touch the monster. Come on, man.\n"
+            )
+
+        # Mipha's attack
+        rollTwo = dice(10)
+        if rollTwo[0] >= 10:
+            print(
+                "\nMipha rolled a staggering " +
+                str(rollTwo[0]) +
+                ", That means she dealt 30 HP to the monster!\n"
+            )
+            seaMonst = seaMonst - int(rollTwo[0])
+        elif rollTwo[0] >= 5:
+            print(
+                "\nMipha rolled " +
+                str(rollTwo[0]) +
+                ", That means she dealt 15 HP to the monster!\n"
+            )
+        elif rollTwo[0] < 5:
+            print(
+                "\nMipha rolled a measly " +
+                str(rollTwo[0]) +
+                ", That means she didn't even touch the monster.\n"
+            )
+
+        # Sea monsters attack
+        rollThree = dice(15)
+
+        # High dmg sea monster attack
+        if rollThree[0] >= 15:
+            print(
+                "\nThe monster hit you both swiftly and hard. -15 HP to both\n"
+            )
+            hp(0, "edit", -15)
+            if int(hp(0, "list", 0)) == 0:
+                print(
+                    "\nThe sea monster tore you both apart. Womp Womp.\n"
+                )
+            mipha = mipha - 15
+            if mipha <= 0:
+                print("Mipha is dying!!!!!")
+
+            # Low dmg sea mosnter attack
+        elif rollThree[0] < 15:
+            print(
+                "\nThe monster swings at you wildly, -5 HP to both.\n"
+            )
+            hp(0, "edit", -5)
+            if int(hp(0, "list", 0)) == 0:
+                print(
+                    "\nThe sea monster tore you both apart. Womp Womp.\n"
+                )
+            mipha = mipha - 5
+            if mipha <= 0:
+                print("Mipha is dying!!!!!")
+
+        round = round + 1
+        print("\n\nNext round here we go!\n\n")
+        time.sleep(5)
+        print("MONSTER HEALTH = " + str(seaMonst))
+        print("\n\n====== ROUND " + str(round) + " ======")
+
+        # Ending of section 5, I would've put it in it's own function, but this is easier.
+        if seaMonst <= 0:
+            print(
+                "\nThrashing wildly, the sea monster slithers back into the lake.\n"
+            )
+            if int(hp(0, "list", 0)) > 0:
+                if mipha > 0:
+                    print(
+                        "\nAfter dealing one last blow to the monster, it finally shrinks back into the lake. You and Mipha both survive. 'Finally, I've been waiting to give you this', says Mipha. She gives you some treasure and some apples for good measure. You leave, and she decides to stay.\n"
+                    )
+                    sectComplete(5)
+            elif int(hp(0, "list", 0)) > 0:
+                if mipha <= 0:
+                    print(
+                        "\nYou're about to finally kill the sea monster, when it hits Mipha. You hit the sea monster one last time, making it finally slither back into the lake. Mipha, with her last dying breath, gives you the treasure. You leave her lifeless body on the raft.\n"
+                    )
+                    sectComplete(5)
+        else:
+            continue
+
+
 #oneStart() <<< what will actually be called in production
 
 if startingSect == 1:
